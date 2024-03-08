0e64b981cbb5f6d3c21d10f6705ce5c4b3c8743b
TessT1ckles <134611258+TessT1ckles@users.noreply.github.com>
2024-03-09 00:18:24 +0800

    Add files via upload

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,19 @@
+function subscribe_pop() {
+    alert("You will now be updated with our latest news to your email. Thank You!.");
+}
+
+function feedback_pop() {
+    if (confirm("Go to our Feedback Hub and share your feedback with us?")) {
+    } 
+    else {
+
+    }
+}
+
+function try_pop() {
+    if (confirm("Are you sure you want to purchase this service?")) {
+    } 
+    else {
+
+    }
+}
